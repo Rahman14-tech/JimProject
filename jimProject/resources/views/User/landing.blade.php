736baa5f644c3a730f01925956dfec05e12658e4
SIDAN <rosidanfaisal@gmail.com>
2023-06-02 18:41:37 +0700

    style

---
+++ jimProject/resources/views/User/landing.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/js/app.js'])
+    <title>Landing Page</title>
+    <style>
+        #border1{
+            border: 1px solid;
+        }
+    </style>
+</head>
+    <body>
+        <div class="container" id="border1">
+            <h1>
+                kocak
+            </h1>
+        </div>
+    </body>
+</html>
+
+
+
+<!-- JS -->
